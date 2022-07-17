fdeb3c9b9f9bf0cbbbc71b239c5bc1dcf4181153
Marcos Gonçalves <marcoslopesg7@gmail.com>
2022-07-17 13:53:02 -0300

    Espaço.

--- views/contrato.dependentes.js
+++ views/contrato.dependentes.js
@@ -43,7 +43,6 @@ function ContratoContentDependentes({ navigation }) {
                 >
                     PROSSEGUIR
                 </Button>
-
             </VStack>
         </ScrollView >
     )
